b3ee6ce6f49ef99a48e382da7ad0d9bf10e05c35
Bruno Pires <bruno.angeloti@terra.com.br>
2021-06-08 17:04:10 -0300

    cardBox pronto, mas nao responsivo

--- pages/index.js
+++ pages/index.js
@@ -22,13 +22,17 @@ export default function Home() {
       </Head>
 
       <div className={styles.header}>
-        <h3 data-aos="fade-right">SOBRE</h3>
-        <h3 data-aos="fade-right">PROJETOS</h3>
+        <img data-aos="fade-right" src="logo.svg" alt="logo angeloti" />
+        <div className={styles.buttons}>
+          <h3 data-aos="fade-right">SOBRE</h3>
+          <h3 data-aos="fade-right">PROJETOS</h3>
+        </div>
+        
       </div>
 
       <div className={styles.home}>
         <div data-aos="fade-right" className={styles.content}>
-          <h1>BRUNO ANGELOTI</h1>
+          <h1>BRUNO <div className={styles.sobrenome}>ANGELOTI</div></h1>
           <h2>Desenvolvedor Frontend e mobile</h2>
         </div>
         <div data-aos="fade-right" className={styles.image}>
@@ -36,7 +40,7 @@ export default function Home() {
         </div>   
       </div>  
 
-      {/*<div className={styles.about}>
+      <div className={styles.about}>
         <div className={styles.card}>
           <CardAbout />
         </div>
@@ -45,8 +49,9 @@ export default function Home() {
           <h2>LINGUAGENS</h2>
           <h2>TECNOLOGIAS</h2>
         </div>
-  </div>*/}
-         <span className={styles.mouse}></span>
+      </div>
+
+      <span className={styles.mouse}></span>
     </div>
       
   )
