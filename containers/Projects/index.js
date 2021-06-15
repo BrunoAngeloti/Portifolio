589afb6a577160c08fa759e38fc6abec153206c0
Bruno Pires <bruno.angeloti@terra.com.br>
2021-06-15 20:34:20 -0300

    Coloquei um novo projeto

--- containers/Projects/index.js
+++ containers/Projects/index.js
@@ -24,8 +24,8 @@ export function Projects(){
                         </a>
                     </div>  
                     <div data-aos="zoom-in">
-                        <a href="https://github.com/BrunoAngeloti/Pong" target="_blanked">
-                            <CardProject title={"PONG"} subtitle={"Jogo Pong feito em javascript puro. Aplicado uma inteligência artificial para jogar contra. Projeto feito para o grupo de IA do PET engenharia de computação - UFES."}/>  
+                        <a href="https://github.com/BrunoAngeloti/NextLevelWeek5" target="_blanked">
+                            <CardProject title={"NLW5"} subtitle={"Aplicativo PlantManager feito para a Next Level Week 5. O aplicativo foi feito com a tecnologia react native, utilizando Expo e desenvolvida em um prazo de 1 semana. Design feito no figma"}/>  
                         </a>
                     </div>      
                 </div>          
